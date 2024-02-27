6b9576a6d523300538eed775b8eacd2b72108f05
BarajasLY <159092229+BarajasLY@users.noreply.github.com>
2024-02-26 22:29:55 -0800

    Function basics

---
+++ Lab4_func.cpp
@@ -0,0 +1,117 @@
+
+#include <iostream>
+using namespace std;
+
+/** sumOfThree
+* @brief returns the sum of x, y, and z
+*
+* @param x : (int)
+* @param y : (int)
+* @param z : (int)
+* @return
+*/
+
+int sumOfThree(int x, int y, int z){
+    return x + y+ z;
+}
+
+/** division
+* @brief returns x / y
+*
+* @param x : (double)
+* @param y : (double)
+* @return double
+*/
+
+double division(double x, double y){
+    return x / y; 
+}
+/** isGreatherThan
+* @brief returns whether x is greater than y
+*
+* @param x : (double)
+* @param y : (int)
+* @return true : x is greater than y
+* @return false : x is less than or equal to y
+*/
+
+bool isGreaterThan(double x, int y){
+    return x > y; 
+}
+
+/** select
+* @brief output the statements based on func_type
+* func_type : output
+* "sum" : "The sum is "
+* "division" : "The quotient is "
+* "comparison" : "The first value being greater than the second value is "
+* All other phrases: "Invalid function type\n"
+* @param func_type : (string)
+* @return string
+*/
+
+string select(string func_type){
+
+    string out = "";
+
+    if (func_type == "sum"){
+        out = "The sum is"; 
+    }
+        
+    else if (func_type == "division"){
+         out = "The quotient is";
+    }
+
+    else if (func_type == "comparison"){
+        out = "The greater value is";
+    }
+
+    else{
+        out = "Invalid";
+    }
+
+    return out;
+       
+}
+
+/**
+* @brief Calls select()
+*
+* @tparam T : (int,double, or string)
+* @param func_type
+* @param output
+*/
+
+template <typename T>
+void myPrint(string func_type, T output){
+    cout << select(func_type) << output << endl;
+}
+
+int main(){
+    //make the 3 integers
+    int x1 = 2;
+    int x2 = 3;
+    int x3 = 5; 
+
+    //call sum of three
+    int sum_out = sumOfThree(x1,x2,x3);
+    //call my print
+    myPrint("sum", sum_out);
+
+    //initialize 2 double parameters
+    double dividend = 12345.1;
+    double divisor = 2345.6;
+
+    //call division
+    double div_out = division(dividend, divisor);
+    myPrint("division", div_out);
+
+    //comapare div_out and sum_out
+    bool is_greater_out = isGreaterThan(div_out, sum_out);
+    myPrint("comparison", is_greater_out);
+
+
+    return 0;
+}
+
+
